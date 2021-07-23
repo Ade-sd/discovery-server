19bf4947eb043eaa8982a3906f17e3d1f1b2ccae
adesd <adesd16@gmail.com>
2021-07-23 12:57:04 +0430

    nars discovery server first commit

---
+++ src/main/java/com/nars/NarsDiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.nars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NarsDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarsDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nars/NarsDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarsDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
